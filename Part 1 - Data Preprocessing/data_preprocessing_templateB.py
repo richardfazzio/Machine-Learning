8c8df61dabeef432a69061132f6eaf45ba3bfafe
richardfazzio <rfazzio@fau.edu>
2018-03-27 21:48:31 -0400

    a

--- Part 1 - Data Preprocessing/data_preprocessing_templateB.py
+++ Part 1 - Data Preprocessing/data_preprocessing_templateB.py
@@ -6,9 +6,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
+
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+#taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer() #missing_values = 'NaN', strategy = 'mean', axis = 0, defaults
+imputer = imputer.fit(X[:, 1:3]) # all rows and columns 1 to 2, upper bound isn't included
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
